1e173a1fa23ab16dd885dd53e9bed6b675745e5d
psalmistcom <dadewale9@gmail.com>
2024-01-09 18:56:51 +0100

    bottom navigation bar done with controller

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:foodly/constants/constants.dart';
 import 'package:foodly/views/entrypoint.dart';
 import 'package:get/get.dart';
 
-Widget defaultHome = const MainScreen();
+Widget defaultHome = MainScreen();
 void main() {
   runApp(const MyApp());
 }
